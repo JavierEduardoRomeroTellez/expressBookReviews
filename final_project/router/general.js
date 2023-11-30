dd4807a28081c96457bf84b3ed7f0181cccae5e7
Javier Romero <jaromero@tpitic.com.mx>
2023-11-30 13:16:48 -0700

    Finished Final Project

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,41 +3,79 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require('axios');
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let username = req.body.username;
+  let password = req.body.password;
+
+  if(username && password){
+    if(!isValid(username)){
+      users.push({"username":username,"password":password});
+      return res.status(200).json({message: "User successfully registred. Now you can login"});
+    } else {
+      return res.status(404).json({message: "User already exists!"});
+    }
+  }
+  return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn;
+  res.send(JSON.stringify(books[isbn], null, 4));
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let nbooks = Object.values(books);
+  let author = req.params.author;
+
+  let filtered_books = nbooks.filter((book) => book.author === author);
+
+  res.send(JSON.stringify(filtered_books, null, 4));
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let nbooks = Object.values(books);
+  let title = req.params.title;
+
+  let filtered_books = nbooks.filter((book) => book.title === title);
+
+  res.send(JSON.stringify(filtered_books, null, 4));
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn;
+  res.send(JSON.stringify({"reviews" : books[isbn].reviews}, null, 4));
 });
+
+const getAllBooks = async () => {
+  const response = await axios.get('http://localhost:3300/');
+  console.log(response.data);
+}
+
+const getBookDetails = async () => {
+  const response = await axios.get('http://localhost:3300/isbn/1');
+  console.log(response.data);
+}
+
+const getDetailsAuthor = async () => {
+  const response = await axios.get('http://localhost:3300/author/Unknown');
+  console.log(response.data);
+}
+
+const getDetailsTitle = async () => {
+  const response = await axios.get('http://localhost:3300/title/Fairy%20tales');
+  console.log(response.data);
+}
 
 module.exports.general = public_users;
